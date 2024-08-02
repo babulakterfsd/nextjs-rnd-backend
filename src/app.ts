41d09835f79e809fcaa6ccf2099479b065a6c9a2
Babul Akter <babulakterfsd@gmail.com>
2024-08-02 18:14:24 +0600

    refreshToken bug fixed

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['https://nextjs-rnd.vercel.app', 'http://localhost:3000'],
+    origin: [
+      'https://nextjs-rnd-babulakterfsd.vercel.app',
+      'http://localhost:3000',
+    ],
     credentials: true,
   }),
 );
